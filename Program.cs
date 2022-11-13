29ca3838b63d4da34d5d3d331715dc4f818ecb39
Alexander Dev <82269375+Alexandersdr@users.noreply.github.com>
2022-11-13 07:46:52 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DotnetLists
+{
+ class Program
+ {
+    static void Main(string[] args)
+    {
+        Console.Clear();
+
+        var meuArray = new int[5] { 1, 2, 3, 4, 5 };
+        meuArray[0] = 12;
+        Console.WriteLine(meuArray.Length);
+
+    }
+ }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# DotnetLists
+ dotnet-6
